b83a5a47a152126f54b8b4ff7ba50a978b963a6c
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-07 12:06:12 -0400

    Added gem 'maxminddb' and establishes local level geocoding

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -9,4 +9,7 @@ Ahoy.api = false
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = false
+Ahoy.geocode = true
+
+#perform geocoding in the background so it doesn't show down web request
+Ahoy.job_queue = :low_priority
