73660be9999d4393d016b1d19d10d8b5351cc4db
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-09 23:05:47 -0400

    Refactored create_visitor_object to use find_or_create_by. Each visit was counted as unique so uses visitor_id as search to count unique visits

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -1,7 +1,6 @@
 class Ahoy::Event < ApplicationRecord
   include Ahoy::QueryMethods
   after_create :create_visitor_object 
-
   self.table_name = "ahoy_events"
 
   belongs_to :visit
@@ -12,10 +11,22 @@ class Ahoy::Event < ApplicationRecord
 
    #link ahoy visitor to visitor
   def create_visitor_object 
-    @visitor = Visitor.new(name: "Anonomous " + Faker::Name.name, first_seen: self.time.to_s,ahoy_visit_id: self.visit_id, app_id:properties["app_id"].to_i) 
-    @page_id = self.properties["id"]
-    @visitor.pages << Page.find(@page_id)
-    @visitor.save
+    #find or create
+
+    @visitor = Visitor.find_or_create_by(ahoy_visit_id: self.visit_id) do |visitor|
+
+      visitor.name = "Anonomous " + Faker::Name.name
+      visitor.first_seen = self.time.to_s
+      visitor.ahoy_visit_id =  self.visit_id
+      visitor.app_id = properties["app_id"].to_i
+      @app = App.find(properties["app_id"])
+      visitor.pages << @app.pages.find(properties["id"])
+    end
+
+    # @visitor = Visitor.new(name: "Anonomous " + Faker::Name.name, first_seen: self.time.to_s,ahoy_visit_id: self.visit_id, app_id:properties["app_id"].to_i) 
+    # @page_id = self.properties["id"]
+    # @visitor.pages << Page.find(@page_id)
+    # @visitor.save
   end
 
   serialize :properties, JSON
