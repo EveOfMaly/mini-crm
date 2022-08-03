993efbaf9164901b7cbaa64c50fc78aee38537fb
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-03 11:00:12 -0700

    switched to master as someone

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -10,6 +10,8 @@ end
 # set to true for JavaScript tracking
 Ahoy.api = true
 
+Ahoy.track_visits_immediately = true
+Ahoy.quiet = false
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
