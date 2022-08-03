4b27831d46949f8921ae83a11d93a9a6ba264bd3
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-03 10:32:56 -0700

    Replaced bio scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import ahoy from "ahoy.js"
+import ahoy from "ahoy.js";
+
 
 Rails.start()
 Turbolinks.start()
