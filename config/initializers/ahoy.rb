5bc2a24b386dfec2a040a309a75adbe93a688552
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-11 11:33:46 -0700

    Remove last commit about settrack visits immediately

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,4 +1,31 @@
 class Ahoy::Store < Ahoy::DatabaseStore
+
+Ahoy.job_queue = :low_priority
+
+def geocode(data)
+  super(data)
+end
+
+def track_visit(data)
+  # new visit
+  super(data)
+end
+
+def track_event(data)
+  # new event
+  super(data)
+end
+
+def geocode(data)
+  # visit geocoded
+  super(data)
+end
+
+def authenticate(data)
+  # user authenticates
+  super(data)
+end
+
 end
 
 # set to true for JavaScript tracking
@@ -8,5 +35,5 @@ Ahoy.api = false
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
 Ahoy.geocode = false
-Ahoy.track_visits_immediately = true
+Ahoy.job_queue = :low_priority
 Ahoy.quiet = false
