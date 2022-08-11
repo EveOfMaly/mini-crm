67dcc6126b7f11561f4934208f23304c7d9d459f
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-11 11:57:16 -0700

    Updated Ahoy::Store with data stores

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import ahoy from "ahoy.js";
+import ahoy from "ahoy.js"
+
+//= require ahoy
 
 
 Rails.start()
--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,13 +1,30 @@
 class Ahoy::Store < Ahoy::DatabaseStore
   mattr_accessor :auto_mount
   self.auto_mount = false
+
+  def track_visit(data)
+    # new visit
+  end
+
+  def track_event(data)
+    # new event
+  end
+
+  def geocode(data)
+    # visit geocoded
+  end
+
+  def authenticate(data)
+    # user authenticates
+  end
 end
 
 # set to true for JavaScript tracking
-Ahoy.api = false
+Ahoy.api = true
 
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = false
+Ahoy.geocode = true
+Ahoy.job_queue = :low_priority
 Ahoy.quiet = false
