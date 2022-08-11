c460e57bef8c80b967ce9ad9c4f4b4c11b0313b4
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-11 11:40:48 -0700

    Updated Ahoy:api to ttrue and add automount code

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,35 +1,17 @@
 class Ahoy::Store < Ahoy::DatabaseStore
+  mattr_accessor :auto_mount
+  self.auto_mount = false
 
 Ahoy.job_queue = :low_priority
 
-def geocode(data)
-  super(data)
-end
-
-def track_visit(data)
-  # new visit
-  super(data)
-end
-
-def track_event(data)
-  # new event
-  super(data)
-end
-
-def geocode(data)
-  # visit geocoded
-  super(data)
-end
-
-def authenticate(data)
-  # user authenticates
-  super(data)
-end
+  def geocode(data)
+    super(data)
+  end
 
 end
 
 # set to true for JavaScript tracking
-Ahoy.api = false
+Ahoy.api = true
 
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
