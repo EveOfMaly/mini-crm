9dd1d83c283675b7aa6536d583ea19f72370efce
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-11 11:47:12 -0700

    Set Ahoy:Api to false

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,21 +1,13 @@
 class Ahoy::Store < Ahoy::DatabaseStore
   mattr_accessor :auto_mount
   self.auto_mount = false
-
-Ahoy.job_queue = :low_priority
-
-  def geocode(data)
-    super(data)
-  end
-
 end
 
 # set to true for JavaScript tracking
-Ahoy.api = true
+Ahoy.api = false
 
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
 Ahoy.geocode = false
-Ahoy.job_queue = :low_priority
 Ahoy.quiet = false
