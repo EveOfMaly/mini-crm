fc1f2f39f40e8226165bd13878297053aa08a57b
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-16 17:34:13 -0700

    Update for production

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,30 +1,31 @@
 class Ahoy::Store < Ahoy::DatabaseStore
   # mattr_accessor :auto_mount
-  # self.auto_mount = false
 
-  # def track_visit(data)
-  #   # new visit
-  # end
 
-  # def track_event(data)
-  #   # new event
-  # end
+  def track_visit(data)
+    # new visit
+  end
 
-  # def geocode(data)
-  #   # visit geocoded
-  # end
+  def track_event(data)
+    # new event
+  end
 
-  # def authenticate(data)
-  #   # user authenticates
-  # end
-  Ahoy.api = true
-  Ahoy.geocode = true
-Ahoy.job_queue = :low_priority
-Ahoy.quiet = false
+  def geocode(data)
+    # visit geocoded
+  end
+
+  def authenticate(data)
+    # user authenticates
+  end
+
 end
 
 # set to true for JavaScript tracking
 Ahoy.api = true #set to false
+Ahoy.geocode = true
+Ahoy.job_queue = :low_priority
+Ahoy.quiet = false
+
 
 
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
