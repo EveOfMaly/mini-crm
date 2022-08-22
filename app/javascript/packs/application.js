be44a6fc789dab5c7b0c2faf605d1128c26d6d90
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-21 21:50:03 -0700

    Comment applicaiton.js in javascript folter

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import ahoy from "ahoy.js"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// // import ahoy from "ahoy.js"
 
 
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 
