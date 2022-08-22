51003c8dbb2d46daf4392f9d37f4cb4cb39337f9
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-21 23:00:37 -0700

    Commmented out create_visitor_object after_save not working

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,24 +1,24 @@
 class Ahoy::Visit < ApplicationRecord
   self.table_name = "ahoy_visits"
-  after_save :create_visitor_object 
+  # after_save :create_visitor_object 
   
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
 
-  def create_visitor_object 
+  # def create_visitor_object 
 
-    @visitor = Visitor.find_or_create_by(ahoy_visit_id: self.id) do |visitor|
-      visitor.name = "Anonomous Visitor " + Faker::Name.name
-      visitor.first_seen = self.started_at
-      visitor.ahoy_visit_id =  self.id
-      app_id = self.landing_page.match(/\d+$/).to_s.to_i
-      visitor.app_id = app_id
+  #   @visitor = Visitor.find_or_create_by(ahoy_visit_id: self.id) do |visitor|
+  #     visitor.name = "Anonomous Visitor " + Faker::Name.name
+  #     visitor.first_seen = self.started_at
+  #     visitor.ahoy_visit_id =  self.id
+  #     app_id = self.landing_page.match(/\d+$/).to_s.to_i
+  #     visitor.app_id = app_id
       
 
-      @app = App.find(app_id)
-      visitor.pages << @app.pages.find(app_id)
-    end
-  end
+  #     @app = App.find(app_id)
+  #     visitor.pages << @app.pages.find(app_id)
+  #   end
+  # end
 
 
 end
